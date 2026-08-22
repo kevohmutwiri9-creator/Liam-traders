06b4e5c38c783630d36313ec8dac185ddc24554a
Kevin <kevohmutwiri35@gmail.com>
2026-08-22 22:34:09 +0300

    Add root URL endpoint to show API information

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,31 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
+
+def api_root(request):
+    return JsonResponse({
+        'message': 'Liam Traders API',
+        'version': '1.0.0',
+        'endpoints': {
+            'admin': '/admin/',
+            'api_docs': '/api/docs/',
+            'api_schema': '/api/schema/',
+            'auth': '/api/auth/',
+            'users': '/api/users/',
+            'tasks': '/api/tasks/',
+            'surveys': '/api/surveys/',
+            'courses': '/api/courses/',
+            'wallet': '/api/wallet/',
+            'payments': '/api/payments/',
+        }
+    })
+
+
 urlpatterns = [
+    path('', api_root),
     path('admin/', admin.site.urls),
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.jwt')),
